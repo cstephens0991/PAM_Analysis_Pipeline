aa0f4ae4ae91e6b4c1f1dadff1bf190710080040
SinaedaA <anderssen.sinaeda@gmail.com>
2024-03-08 11:11:12 +0100

    tested and debugged combine_output_v3.py

--- PAM_analysis_packages/combine_data_v3.py
+++ PAM_analysis_packages/combine_data_v3.py
@@ -7,22 +7,23 @@ import os
 
 def main():
     fvfm, pa_dir, outpath = parsing_arguments()
-    ## Concatenate the plant area data
-    Plant_area_files_list = glob.glob(f"{pa_dir}*_threshold_image.csv")
-    plant_area_data = concat_plant_area_data(Plant_area_files_list)
 
     ## Read the FvFm data
     fvfm_data = pd.read_csv(f"{fvfm}")
+    unique_plate_names = fvfm_data["Plate"].unique()
+
+    ## Concatenate the plant area data
+    plant_area_data = concat_plant_area_data(pa_dir, unique_plate_names)
 
     ## Check the plate name correspondance
     check_plate_names(plant_area_data, fvfm_data)
 
     ## Merge the data and dropped some of the columns
     merged_data = plant_area_data.merge(fvfm_data, how="inner", on=["Plate", "Well"])
-    merged_data = merged_data.drop(columns=["Count", "Average Size", "%Area", "Mean", "Unnamed: 0"])
+    merged_data = merged_data.drop(columns=["Count", "Average_Size", "%Area", "Mean", "Unnamed: 0"])
 
     # Combine ImageJ area score and FvFm score into a single value
-    merged_data["Area_FvFm"] = merged_data["Total Area"] * merged_data["FvFm"]
+    merged_data["Area_FvFm"] = merged_data["Total_Area"] * merged_data["FvFm"]
     merged_data.to_csv(f"{outpath}/combined_output.csv")
 
 def parsing_arguments():
@@ -30,8 +31,8 @@ def parsing_arguments():
         prog='combine_data_v3.py',
         description='This script is used to combine data from ImageJ (Plant Area) and the fvfm data (from get_fvfm_v3.py).')
     parser.add_argument('--fvfm-file', help="Path to the output csv file of get_fvfm_v3.py. This is a necessary argument.")
-    parser.add_argument('--pa-dir', default = "./Plant_area_data/",
-                        help="Path to directory containing the *.tif files. [default = './Plant_area_data/']")
+    parser.add_argument('--pa-dir', default = "./plant_area_data/",
+                        help="Path to directory containing the *.tif files. [default = './plant_area_data/']")
     parser.add_argument('--outpath', help=f"Path to output directory which will contain the combined_output.csv file.")
     args = vars(parser.parse_args())
     fvfm = os.path.abspath(args["fvfm_file"])
@@ -40,16 +41,17 @@ def parsing_arguments():
 
     return fvfm, pa_dir, outpath
 
-def concat_plant_area_data(Plant_area_files_list):
-    total_data = pd.DataFrame([], columns=["Plate", "Count", "Total Area", "Average Size", "%Area", "Mean"])
-    for area_file in Plant_area_files_list:
-        plate_name = area_file.split("./Plant_area_data\\")[1]
-        plate_name = plate_name.split("_threshold_image.csv")[0]
-        plate_data = pd.read_csv(area_file)
+def concat_plant_area_data(pa_dir, plate_names):
+    total_data = pd.DataFrame([], columns=["Plate", "Count", "Total_Area", "Average_Size", "%Area", "Mean"])
+    for plate_name in plate_names:
+        ## open file with the plate name
+        plate_data = pd.read_csv(f"{pa_dir}/{plate_name}_threshold_image.csv")
         if plate_data.shape[0] != 24:
             print(f"{plate_name} length is {plate_data.shape[0]}")
         plate_data["Plate"] = plate_name
-        plate_data['Well'] = np.arange(1, (len(plate_data)+1)).astype(int)
+        plate_data["Well"] = np.arange(1, (len(plate_data)+1)).astype(int)
+        ## add "Well" in front of numbers in Well column
+        plate_data.Well = plate_data.Well.apply(lambda x: '_'.join(["Well", str(x)]))
         plate_data = plate_data.drop(columns="Slice")
         total_data = pd.concat([total_data, plate_data], axis=0)
     return total_data
